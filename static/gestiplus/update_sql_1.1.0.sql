bbdbdea2e28274e4f5d3e4fccd7a74a027db1d0b
Williams Orellana <williams.orellana@gmail.com>
2021-12-29 12:54:09 -0600

    Actualizacion archivo SQL

--- static/gestiplus/update_sql_1.1.0.sql
+++ static/gestiplus/update_sql_1.1.0.sql
@@ -1,7 +1,9 @@
-ALTER TABLE "dbo"."empresas" ADD "Representante" NCHAR(50) NOT NULL DEFAULT '';
-ALTER TABLE "dbo"."empresas" ADD "Giro" NCHAR(100) NOT NULL DEFAULT '';
-ALTER TABLE "dbo"."empresas" ADD "Direccion" NCHAR(150) NOT NULL DEFAULT '';
-ALTER TABLE "dbo"."empresas" ADD "Resolucion" NCHAR(150) NOT NULL DEFAULT '';
-ALTER TABLE "dbo"."empresas" ADD "DeResolucion" NCHAR(75) NOT NULL DEFAULT '';
-ALTER TABLE "dbo"."empresas" ADD "AResolucion" NCHAR(75) NOT NULL DEFAULT '';  
-ALTER TABLE "dbo"."empresas" ADD "FechaResolucion" DATE NOT NULL DEFAULT 'getdate()';
+use gestiplus
+GO
+ALTER TABLE "dbo"."empresas" ADD "Representante" NCHAR(50) NOT NULL DEFAULT ''
+ALTER TABLE "dbo"."empresas" ADD "Giro" NCHAR(100) NOT NULL DEFAULT ''
+ALTER TABLE "dbo"."empresas" ADD "Direccion" NCHAR(150) NOT NULL DEFAULT ''
+ALTER TABLE "dbo"."empresas" ADD "Resolucion" NCHAR(150) NOT NULL DEFAULT ''
+ALTER TABLE "dbo"."empresas" ADD "DeResolucion" NCHAR(75) NOT NULL DEFAULT ''
+ALTER TABLE "dbo"."empresas" ADD "AResolucion" NCHAR(75) NOT NULL DEFAULT ''  
+ALTER TABLE "dbo"."empresas" ADD "FechaResolucion" DATE NOT NULL DEFAULT 'getdate()'
