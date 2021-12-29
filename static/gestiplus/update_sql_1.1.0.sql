8320165ddb68bab49759876a2c5c6e625b0d2f1f
Williams Orellana <williams.orellana@gmail.com>
2021-12-29 14:47:52 -0600

    Se agrega el ultimo campo de base de datos

--- static/gestiplus/update_sql_1.1.0.sql
+++ static/gestiplus/update_sql_1.1.0.sql
@@ -7,3 +7,5 @@ ALTER TABLE [empresas] ADD "Resolucion" NCHAR(150) NOT NULL DEFAULT ''
 ALTER TABLE [empresas] ADD "DeResolucion" NCHAR(75) NOT NULL DEFAULT ''
 ALTER TABLE [empresas] ADD "AResolucion" NCHAR(75) NOT NULL DEFAULT ''  
 ALTER TABLE [empresas] ADD "FechaResolucion" DATE NOT NULL DEFAULT GETDATE()
+
+ALTER TABLE [productos] ALTER COLUMN "nombre" NCHAR(60)
