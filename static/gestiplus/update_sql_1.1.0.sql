e4824ac726a04d90152488ffefff6ca2740adaa3
Williams Orellana <williams.orellana@gmail.com>
2021-12-29 13:07:46 -0600

    Se ajusta el archivo SQL

--- static/gestiplus/update_sql_1.1.0.sql
+++ static/gestiplus/update_sql_1.1.0.sql
@@ -1,9 +1,9 @@
-use gestiplus
+use [gestiplus]
 GO
-ALTER TABLE "dbo"."empresas" ADD "Representante" NCHAR(50) NOT NULL DEFAULT ''
-ALTER TABLE "dbo"."empresas" ADD "Giro" NCHAR(100) NOT NULL DEFAULT ''
-ALTER TABLE "dbo"."empresas" ADD "Direccion" NCHAR(150) NOT NULL DEFAULT ''
-ALTER TABLE "dbo"."empresas" ADD "Resolucion" NCHAR(150) NOT NULL DEFAULT ''
-ALTER TABLE "dbo"."empresas" ADD "DeResolucion" NCHAR(75) NOT NULL DEFAULT ''
-ALTER TABLE "dbo"."empresas" ADD "AResolucion" NCHAR(75) NOT NULL DEFAULT ''  
-ALTER TABLE "dbo"."empresas" ADD "FechaResolucion" DATE NOT NULL DEFAULT 'getdate()'
+ALTER TABLE [empresas] ADD "Representante" NCHAR(50) NOT NULL DEFAULT ''
+ALTER TABLE [empresas] ADD "Giro" NCHAR(100) NOT NULL DEFAULT ''
+ALTER TABLE [empresas] ADD "Direccion" NCHAR(150) NOT NULL DEFAULT ''
+ALTER TABLE [empresas] ADD "Resolucion" NCHAR(150) NOT NULL DEFAULT ''
+ALTER TABLE [empresas] ADD "DeResolucion" NCHAR(75) NOT NULL DEFAULT ''
+ALTER TABLE [empresas] ADD "AResolucion" NCHAR(75) NOT NULL DEFAULT ''  
+ALTER TABLE [empresas] ADD "FechaResolucion" DATE NOT NULL DEFAULT GETDATE()
